132cc33d738e34fc549e6d1cef842e1b3ccfdaca
samuel <romerotitosamuel@gmail.com>
2022-01-10 19:05:05 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import firebaseApp from './credentials'
 import {getFirestore, collection, doc, setDoc , getDoc, getDocs} from 'firebase/firestore'
 const db = getFirestore(firebaseApp)
 
-
 function App() {
+  // USESTATES
+  const [cancionesDomingo, setCancionesDomingo] = useState([])
   const [listadoIds, setListadoIds] = useState([])
   const [listadoTotal, setListadoTotal] = useState([])
-  const [prDom, setPrDom] = useState([])
-  const cancionesRef = collection(db, 'canciones')
   const [toSave, setToSave] = useState(null)
   
   const submitHandler = async (e) => {
@@ -19,9 +18,37 @@ function App() {
     let contenido = e.target.contenido.value
     
     const objFromForm = { titulo, artista, contenido }
+    const cancionesRef = collection(db, 'canciones')
     setToSave(objFromForm)
     await setDoc(doc(cancionesRef), objFromForm)
   }
+  
+  
+  const domingoRef = collection(db, 'domingo')
+  const addToSun =  async (key) => {
+    await setDoc(doc(domingoRef) , {can: key} )
+  }
+  
+  //ADQUIRIR DATOS --------------------------------------------------
+  const getSoloDom = async () => {
+    const querySnapshot = await getDocs(collection(db, "domingo"))
+    querySnapshot.forEach((dd)=> {
+      setListadoIds(listadoIds.push(dd.data().can))
+    })
+  } 
+  const printDomingo = async () => {
+    await getSoloDom()
+    
+    let acuSongs = []
+    listadoIds.forEach( async (rr) => {
+      const docRef = doc(db, "canciones" , rr)
+      const docSnap = await getDoc(docRef)
+      acuSongs.push(docSnap.data())
+    })
+    setCancionesDomingo(acuSongs)
+  }
+  useEffect( ()=> printDomingo(), []) 
+  
   
   //GENERA LA LISTA DEL INICIO
   const inicioPag = async () => {
@@ -38,55 +65,26 @@ function App() {
       let id = {id: doc.id}
       docComp = Object.assign(id, doc.data())
       docTotal.push(docComp)
-      console.log(docTotal)
     })
     setListadoTotal(docTotal) //lo guarda en el useState
-    console.log(listadoTotal)
+    
   }
-  //useEffect(()=>{ inicioPag() }, [])
+  //useEffect(()=> inicioPag() , [])
+
+  const ejecutar = () => {
+    console.log(cancionesDomingo)
+  }
   
-  const domingoRef = collection(db, 'domingo')
-  const addToSun =  async (key) => {
-    await setDoc(doc(domingoRef) , {can: key} )
-  }
+  const rep = () => {cancionesDomingo.map((yy) => {
+    console.log(yy.titulo)
+    return (<div>{yy.titulo}</div>)
 
-  const printDomingo = () => {
-    let domObjects = []
-    listadoIds.forEach( async (rr) => {
-      const rrstr = String(rr) 
-      
-      const docRef = doc(db, "domingo" , rrstr)
-      const docSnap = await getDoc(docRef)
-      //domObjects.push(docSnap.data())
+  })
 
-      if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-      }
-
-
-      console.log(docSnap.data())
-    })
   }
 
-  const getSoloDom = async () => {
-    const querySnapshot = await getDocs(collection(db, "domingo"))
-    let listDomIds = []
-    querySnapshot.forEach((dd)=> {
-      listDomIds.push(dd.data().can)
-    })
-    setListadoIds(listDomIds)
-  } 
-  useEffect( ()=> getSoloDom(), [])
-  
-  printDomingo()
-  //useEffect( ()=> printDomingo(), []) 
-  
 
-
-  return (
+  return  (
     <div>
       <h1>Hola hola</h1>
       <form onSubmit={submitHandler}>
@@ -99,19 +97,28 @@ function App() {
 
       <hr />
 
-      <h1>Lista Dom</h1>
+      <button onClick={ejecutar}>Ejecutar</button>
+      
+      {setTimeout(rep, 1000)}
+      {console.log('count')}
+
 
       <hr />
       <hr />
+
+
       {listadoTotal.map((dd) => {
+        
         return (<div key={dd.id}>
           <button onClick={() => addToSun(dd.id) }>add</button>
           <h4>{dd.titulo} - {dd.artista}</h4>
           <pre><b>{dd.contenido}</b> </pre>
-  
           <hr />
         </div>)
       })}
+
+
+
     </div>
   );
 }

d66ef63c096699f0c5caebea025c79fa357c2bc8
tsamuel1289 <romerotitosamuel@gmail.com>
2022-01-31 14:29:23 -0400

    u

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React  from 'react';
 import './styles/style.scss'
 import { BrowserRouter , Routes, Route, Link } from 'react-router-dom';
+
 //COMPONENTES
 import Iniciopractica from './components/Iniciopractica'
 import Nuevohome from './components/Nuevohome'
 import Navbar from './components/Navbar'
 import Contenido from './components/Contenido'
-import click1 from './styles/click1.mp3'
+
 
 function App() {
-  let bpm = (60 /120) *1000
-  Audio(click1)
-  //setInterval(()=> beat.play() , 1000 )
 
+  //let bpm = (60 /120) *1000
+  //Audio(click1)
+  //setInterval(()=> beat.play() , 1000 )
+  
   return  (<>
-    
     <BrowserRouter>
       <Navbar />
       
       <Routes>
+    
         <Route path="/" element={<Nuevohome/>} /> 
         <Route path="practica" element={<Iniciopractica/>} />
         <Route path="contenido" element={<Contenido/>} />
--- src/components/Nuevohome.js
+++ src/components/Nuevohome.js
@@ -1,14 +1,37 @@
-import React , {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import NoSleep from 'nosleep.js'
 import firebaseApp from "../credentials"
-import {getFirestore, doc, getDoc} from 'firebase/firestore'
-import {Link} from 'react-router-dom'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
+import { Link } from 'react-router-dom'
+
+import useSound from 'use-sound'
+import click1 from '../styles/click1.mp3'
 
 const db = getFirestore(firebaseApp)
 
 
 const Nuevohome = () => {
 
+    const [play] = useSound(click1)
+
+    let son = null
+    let change = false
+    const intervalo = (r) => {
+
+        if (r) {
+
+            son = setInterval(() => play(), 1000)
+            change = true
+        }else{
+            clearInterval(son)
+            change = false
+        }
+
+    
+
+    }
+
+    
     /*var noSleep = new NoSleep();
     document.addEventListener('click', function enableNoSleep() {
         document.removeEventListener('click', enableNoSleep, false);
@@ -22,34 +45,35 @@ const Nuevohome = () => {
         let arraySnap = await getDoc(doc(db, 'domingo', 'dom'))
         let arraySongs = arraySnap.data().array
 
-        arraySongs.forEach( async (song) => {
+        arraySongs.forEach(async (song) => {
             let songSnap = await getDoc(doc(db, 'canciones', song))
-            let songwId = Object.assign(songSnap.data() , {id: song})
-            setDataDom(dataDom => [...dataDom, songwId ])
+            let songwId = Object.assign(songSnap.data(), { id: song })
+            setDataDom(dataDom => [...dataDom, songwId])
         })
     }
-    useEffect( () => getDom(), [] )
+    useEffect(() => getDom(), [])
 
     const enviarSong = (titulo, artista, bpm, url, letra, acordes) => {
-        const song = {titulo, artista, bpm, url, letra, acordes}
-        localStorage.setItem('song' , JSON.stringify(song))
-    }    
-    
-    return(<>
+        const song = { titulo, artista, bpm, url, letra, acordes }
+        localStorage.setItem('song', JSON.stringify(song))
+    }
+
+    return (<>
+        <button onClick={() => intervalo(!change)}>sonido</button>
         {
-            dataDom.map( (ss) => {
-                return(<>    
-                    <Link key={ss.id}  to = {'/contenido'} className = 'listaDom'>
-                        <div  onClick={() => enviarSong(ss.titulo, ss.artista, ss.bpm, ss.url, ss.letra, ss.acordes)}>
-                            <b>{ss.titulo}</b><br /><small>{ss.artista}</small><hr />  
+            dataDom.map((ss) => {
+                return (<>
+                    <Link key={ss.id} to={'/contenido'} className='listaDom'>
+                        <div onClick={() => enviarSong(ss.titulo, ss.artista, ss.bpm, ss.url, ss.letra, ss.acordes)}>
+                            <b>{ss.titulo}</b><br /><small>{ss.artista}</small><hr />
                         </div>
-                    </Link> 
+                    </Link>
                 </>)
             })
         }
-        
-        
+
+
     </>)
-    
+
 }
 export default Nuevohome

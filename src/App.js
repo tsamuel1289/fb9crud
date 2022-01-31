ceef74d89ab815a815dc10d41b20e327c5f807e4
Samuel <romerotitosamuel@gmail.com>
2022-01-30 22:31:52 -0400

    u

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Iniciopractica from './components/Iniciopractica'
 import Nuevohome from './components/Nuevohome'
 import Navbar from './components/Navbar'
 import Contenido from './components/Contenido'
+import click1 from './styles/click1.mp3'
 
 function App() {
-  
+  let bpm = (60 /120) *1000
+  Audio(click1)
+  //setInterval(()=> beat.play() , 1000 )
+
   return  (<>
     
     <BrowserRouter>
